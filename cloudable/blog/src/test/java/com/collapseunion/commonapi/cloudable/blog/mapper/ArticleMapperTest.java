87e02a9584581d6f50199a817c93df8eeedf3fed
CloudSen <www.402645063@gmail.com>
2019-08-21 02:04:03 +0800

    test(blog): add article test

--- cloudable/blog/src/test/java/com/collapseunion/commonapi/cloudable/blog/mapper/ArticleMapperTest.java
+++ cloudable/blog/src/test/java/com/collapseunion/commonapi/cloudable/blog/mapper/ArticleMapperTest.java
@@ -3,6 +3,8 @@ package com.collapseunion.commonapi.cloudable.blog.mapper;
 import com.cloudable.blog.BlogApplication;
 import com.collapseunion.commonapi.cloudable.blog.dto.ArticleSummaryDto;
 import com.collapseunion.commonapi.cloudable.blog.entity.Article;
+import com.collapseunion.commonapi.cloudable.blog.entity.ArticleTag;
+import com.collapseunion.commonapi.cloudable.blog.entity.Tag;
 import com.collapseunion.commonutils.EntityAndDtoConversion;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -19,6 +21,10 @@ public class ArticleMapperTest {
 
     @Autowired
     private ArticleMapper articleMapper;
+    @Autowired
+    private TagMapper tagMapper;
+    @Autowired
+    private ArticleTagMapper articleTagMapper;
 
     @Test
     public void getList() {
@@ -41,6 +47,24 @@ public class ArticleMapperTest {
                 .setImgUrl("https://test.com")
                 .setImgUrlMd("https://test-md.com");
         int insertRows = this.articleMapper.insert(article);
+        Assertions.assertThat(insertRows).isEqualTo(1);
+    }
+
+    @Test
+    public void createNewTag() {
+        Tag tag = new Tag()
+                .setColor("light-green accent-2")
+                .setName("VUE");
+        int insertRows = this.tagMapper.insert(tag);
+        Assertions.assertThat(insertRows).isEqualTo(1);
+    }
+
+    @Test
+    public void associateArticleAndTag() {
+        ArticleTag articleTag = new ArticleTag()
+                .setArticleId("c643f124c139bb4b31f15247fd38aa83")
+                .setTagId("76f86a7e842ede69627e3f76b4a46392");
+        int insertRows = this.articleTagMapper.insert(articleTag);
         Assertions.assertThat(insertRows).isEqualTo(1);
     }
 }
