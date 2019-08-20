61d6c6af37bcad76a830b9114ef3f2c0d603b9b6
CloudSen <www.402645063@gmail.com>
2019-08-21 02:03:07 +0800

    refactor(common-api): finish article query

--- common-api/src/main/java/com/collapseunion/commonapi/cloudable/blog/mapper/ArticleMapper.java
+++ common-api/src/main/java/com/collapseunion/commonapi/cloudable/blog/mapper/ArticleMapper.java
@@ -1,8 +1,11 @@
 package com.collapseunion.commonapi.cloudable.blog.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.collapseunion.commonapi.cloudable.blog.dto.ArticleSummaryDto;
 import com.collapseunion.commonapi.cloudable.blog.entity.Article;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -17,9 +20,32 @@ import java.util.List;
 public interface ArticleMapper extends BaseMapper<Article> {
 
     /**
+     * <p>
      * 获取所有文章摘要列表，包括评论数，和标签
+     * </p>
      *
      * @return 所有文章摘要列表
      */
     List<ArticleSummaryDto> listAllArticleSummary();
+
+    /**
+     * <p>
+     * 分页查询文章摘要，不给查询条件就是分页全部文章摘要
+     * </p>
+     *
+     * @param page      分页参数
+     * @param condition 查询条件
+     * @return 分页后的数据
+     */
+    IPage<ArticleSummaryDto> pageArticleSummaryByCondition(Page<ArticleSummaryDto> page, @Param("condition") ArticleSummaryDto condition);
+
+    /**
+     * <p>
+     * 根据条件计算文章的条数，用于一对多分页查询。
+     * </p>
+     *
+     * @param condition 查询条件
+     * @return 文章条数
+     */
+    Integer countArticleByCondition(@Param("condition") ArticleSummaryDto condition);
 }
