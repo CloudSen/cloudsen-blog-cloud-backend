9ed56455fca5b4cd038c191e71775e1bb1f08a8d
CloudSen <www.402645063@gmail.com>
2019-08-21 02:05:25 +0800

    refactor(blog): add PerformanceInterceptor

--- cloudable/blog/src/main/java/com/cloudable/blog/config/mybatis/MybatisPlusConfig.java
+++ cloudable/blog/src/main/java/com/cloudable/blog/config/mybatis/MybatisPlusConfig.java
@@ -1,15 +1,19 @@
 package com.cloudable.blog.config.mybatis;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import com.cloudable.blog.config.dbconfig.DataSourceConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
 
 /**
  * mybatis plus配置
@@ -47,5 +51,20 @@ public class MybatisPlusConfig {
     public PaginationInterceptor paginationInterceptor() {
         // 最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制 paginationInterceptor.setLimit()
         return new PaginationInterceptor();
+    }
+
+    /**
+     * 性能分析插件
+     */
+    @Bean
+    @Profile({"dev", "test"})
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        //格式化sql语句
+        Properties properties = new Properties();
+        properties.setProperty("format", "true");
+        properties.setProperty("maxTime", "10000");
+        performanceInterceptor.setProperties(properties);
+        return performanceInterceptor;
     }
 }
