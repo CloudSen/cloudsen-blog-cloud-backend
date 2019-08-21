9a3fc0b023e4605ea7304fc7bf4e510e307e7274
CloudSen <www.402645063@gmail.com>
2019-08-22 03:59:39 +0800

    feat(blog): add service constants class

---
+++ cloudable/blog/src/main/java/com/cloudable/blog/service/impl/Constants.java
@@ -0,0 +1,12 @@
+package com.cloudable.blog.service.impl;
+
+/**
+ * ServiceImpl常量类
+ *
+ * @author CloudSen
+ */
+class Constants {
+    static final Integer WARN_DURATION_DAYS = 150;
+    static final String ARTICLE_DURATION_DAYS = "文章《{}》距离上次更新已过 {} 天";
+    static final String ARTICLE_CONTENT = "文章《{}》的内容为: {}";
+}
